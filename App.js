1dcef84b137d554c1a7b8aa40cdb096c5af533fd
sujinee01 <147419514+sujinee01@users.noreply.github.com>
2023-10-21 01:19:23 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -30,10 +30,28 @@ function App() {
     return `${hour} : ${minute} : ${second} ${period}`;
   };
 
+  const [notifications, setNotifications] = useState([]); // 알림을 저장할 상태
+
+  // 새로운 알림이 도착했을 때 호출되는 함수
+  const handleNewNotification = () => {
+    const newNotification = {
+      id: new Date().getTime(),
+      message: "새로운 알림이 도착했습니다!",
+    };
+    setNotifications([...notifications, newNotification]);
+  };
+
+  // 알림을 닫는 함수
+  const closeNotification = (id) => {
+    setNotifications(
+      notifications.filter((notification) => notification.id !== id)
+    );
+  };
+
   return (
     <div className="admin_body">
-      <div class="line-right">
-        <header class="admin_header">
+      <header>
+        <div class="admin_header">
           <div class="main-title">
             <span>
               바른생각
@@ -42,44 +60,51 @@ function App() {
             </span>
           </div>
           <div className="digital-clock">{formattedTime()}</div>
-        </header>
-        <nav class="admin_nav">
-          <div class="info_table">
-            <p class="info_title">남자 테이블 수</p>
-            <div class="info_num">1</div>
-          </div>
-          <div class="info_table">
-            <p class="info_title">여자 테이블 수</p>
-            <div class="info_num">1</div>
-          </div>
-          <div class="info_table">
-            <p class="info_title">혼성 테이블 수</p>
-            <div class="info_num">1</div>
-          </div>
-          <div class="info_table">
-            <p class="info_title">합성 테이블 수</p>
-            <div class="info_num">1</div>
-          </div>
-          <div class="info_table">
-            <p class="info_title">빈 테이블 수</p>
-            <div class="info_num">1</div>
-          </div>
-        </nav>
-        <div class="admin_container">
-          <button class="table_choice">선택</button>
         </div>
-
-        <footer>
-          <div class="admin-footer">
-            <div class="footer-button">
-              <button class="time-plus">시간 추가</button>
-              <button class="heart-plus">하트 충전</button>
-              <button class="table-exit">퇴장 처리</button>
-              <button class="table-mix">합석 처리</button>
-            </div>
-          </div>
-        </footer>
+        <div class="title-alarm">
+          <p class="title-notice">
+            <strong>NOTICE</strong>
+            <img className="bell-image" alt="bell" src="./public/bell.png" />
+          </p>
+        </div>
+        <div class="bottom-line"></div>
+      </header>
+      <nav class="admin_nav">
+        <div class="info_table">
+          <p class="info_title">남자 테이블 수</p>
+          <div class="info_num">1</div>
+        </div>
+        <div class="info_table">
+          <p class="info_title">여자 테이블 수</p>
+          <div class="info_num">1</div>
+        </div>
+        <div class="info_table">
+          <p class="info_title">혼성 테이블 수</p>
+          <div class="info_num">1</div>
+        </div>
+        <div class="info_table">
+          <p class="info_title">합성 테이블 수</p>
+          <div class="info_num">1</div>
+        </div>
+        <div class="info_table">
+          <p class="info_title">빈 테이블 수</p>
+          <div class="info_num">1</div>
+        </div>
+      </nav>
+      <div class="admin_container">
+        <button class="table_choice">선택</button>
       </div>
+
+      <footer>
+        <div class="admin-footer">
+          <div class="footer-button">
+            <button class="time-plus">시간 추가</button>
+            <button class="heart-plus">하트 충전</button>
+            <button class="table-exit">퇴장 처리</button>
+            <button class="table-mix">합석 처리</button>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
